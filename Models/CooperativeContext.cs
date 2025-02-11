1a4d28d9416a596a15ff1379d6d65fcf10430ed4
John Carlo Sinoy <jcsinoy91@gmail.com>
2025-02-10 18:42:11 -0700

    Initial commit

---
+++ Models/CooperativeContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Cooperative_Financing.Models
+{
+    public class CooperativeContext : DbContext
+    {
+        public CooperativeContext(DbContextOptions<CooperativeContext> options)
+            : base(options)
+        {
+        }
+    }
+}
